06406e11188555cfa1552ad1a7c59527741344c9
Kevinjohny111dot <kevinjohny111@gmail.com>
2026-07-01 10:33:56 +0530

    alertchanged1

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: "http://13.220.97.216:8000",
 });
 
+// Attach token to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
