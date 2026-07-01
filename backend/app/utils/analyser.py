5a7d46185c2fecf24b930928bd31f7fec8fd374f
Kevinjohny111dot <kevinjohny111@gmail.com>
2026-07-01 11:29:40 +0530

    skill updated

--- backend/app/utils/analyser.py
+++ backend/app/utils/analyser.py
@@ -1,14 +1,64 @@
 
 def analyze_resume(text: str):
     skills = [
-        "python",
-        "fastapi",
-        "react",
-        "postgresql",
-        "sql",
-        "git",
-        "docker"
-    ]
+    # Programming Languages
+    "python", "java", "javascript", "typescript", "c", "c++", "c#", "go",
+    "php", "ruby", "swift", "kotlin", "rust", "r", "scala",
+
+    # Frontend
+    "html", "css", "react", "angular", "vue", "next.js", "nuxt.js",
+    "bootstrap", "tailwind", "material ui", "redux", "jquery",
+
+    # Backend
+    "fastapi", "django", "flask", "spring", "spring boot",
+    "node.js", "express", "nestjs", "laravel", "asp.net", ".net",
+
+    # Databases
+    "sql", "mysql", "postgresql", "mongodb", "redis", "sqlite",
+    "oracle", "firebase", "cassandra", "elasticsearch",
+
+    # Cloud
+    "aws", "azure", "gcp", "digitalocean", "heroku", "vercel",
+
+    # DevOps
+    "docker", "kubernetes", "jenkins", "terraform", "ansible",
+    "github actions", "gitlab ci", "nginx", "apache",
+
+    # Version Control
+    "git", "github", "bitbucket",
+
+    # Operating Systems
+    "linux", "ubuntu", "windows", "unix",
+
+    # AI / ML / Data Science
+    "tensorflow", "pytorch", "keras", "scikit-learn",
+    "opencv", "pandas", "numpy", "matplotlib",
+    "seaborn", "xgboost", "langchain", "huggingface",
+
+    # APIs
+    "rest api", "graphql", "grpc", "postman", "swagger", "openapi",
+
+    # Mobile
+    "android", "ios", "flutter", "react native",
+
+    # Testing
+    "pytest", "jest", "junit", "selenium", "cypress",
+
+    # Message Brokers
+    "kafka", "rabbitmq",
+
+    # Tools
+    "jira", "confluence", "figma", "notion", "slack",
+
+    # Security
+    "jwt", "oauth", "oauth2", "bcrypt", "ssl", "https",
+
+    # Data Engineering
+    "spark", "hadoop", "airflow",
+
+    # ERP / CRM
+    "erpnext", "odoo", "salesforce"
+]
 
     text = text.lower()
 
@@ -32,16 +82,64 @@ def analyze_job_match(
     job_description: str
 ):
     skills = [
-        "python",
-        "fastapi",
-        "react",
-        "postgresql",
-        "sql",
-        "git",
-        "docker",
-        "aws"
-    ]
+    # Programming Languages
+    "python", "java", "javascript", "typescript", "c", "c++", "c#", "go",
+    "php", "ruby", "swift", "kotlin", "rust", "r", "scala",
 
+    # Frontend
+    "html", "css", "react", "angular", "vue", "next.js", "nuxt.js",
+    "bootstrap", "tailwind", "material ui", "redux", "jquery",
+
+    # Backend
+    "fastapi", "django", "flask", "spring", "spring boot",
+    "node.js", "express", "nestjs", "laravel", "asp.net", ".net",
+
+    # Databases
+    "sql", "mysql", "postgresql", "mongodb", "redis", "sqlite",
+    "oracle", "firebase", "cassandra", "elasticsearch",
+
+    # Cloud
+    "aws", "azure", "gcp", "digitalocean", "heroku", "vercel",
+
+    # DevOps
+    "docker", "kubernetes", "jenkins", "terraform", "ansible",
+    "github actions", "gitlab ci", "nginx", "apache",
+
+    # Version Control
+    "git", "github", "bitbucket",
+
+    # Operating Systems
+    "linux", "ubuntu", "windows", "unix",
+
+    # AI / ML / Data Science
+    "tensorflow", "pytorch", "keras", "scikit-learn",
+    "opencv", "pandas", "numpy", "matplotlib",
+    "seaborn", "xgboost", "langchain", "huggingface",
+
+    # APIs
+    "rest api", "graphql", "grpc", "postman", "swagger", "openapi",
+
+    # Mobile
+    "android", "ios", "flutter", "react native",
+
+    # Testing
+    "pytest", "jest", "junit", "selenium", "cypress",
+
+    # Message Brokers
+    "kafka", "rabbitmq",
+
+    # Tools
+    "jira", "confluence", "figma", "notion", "slack",
+
+    # Security
+    "jwt", "oauth", "oauth2", "bcrypt", "ssl", "https",
+
+    # Data Engineering
+    "spark", "hadoop", "airflow",
+
+    # ERP / CRM
+    "erpnext", "odoo", "salesforce"
+]
     resume_text = resume_text.lower()
     job_description = job_description.lower()
 
